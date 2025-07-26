d1b8756f63170fcf8ad88e4af2e377b771972932
tanuja-13 <tg1644@srmist.edu.in>
2025-07-26 11:22:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def predict():
     return jsonify({"predicted_delivery_time": prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
